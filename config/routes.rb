48aed3a23167370ca12971a879c693fc00b5552b
Taaku0405 <rakosutaa@gmail.com>
2022-06-03 08:41:05 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,35 @@
 Rails.application.routes.draw do
   devise_for :end_users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: "public/sessions"
   }
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+   namespace :admin do
+     resources :end_users, only: [:index, :show, :edit, :update]
+
+     resources :genres, only: [:index, :create, :edit, :update]
+
+     resources :posts, except: [:create]
+
+     get "search" => "searches#search"
+  end
+
+    scope module: :public do
+     resources :end_users, only: [:index, :show, :edit, :update ]
+     get "end_users/unsubscribe", as: "unsubscribe"
+     patch "end_users/withdraw", as: "withdraw"
+
+     resources :posts do
+       resources :post_comments, only: [:create, :destroy]
+       resource :favorites, only: [:create, :destroy]
+     end
+
+     resources :groups, except: [:destroy]
+      get 'search' => 'searches#search'
+   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
