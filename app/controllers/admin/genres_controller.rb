ae3229c2f9776332710f651756a84c319a254230
Taaku0405 <rakosutaa@gmail.com>
2022-06-05 16:51:23 +0000

    アドミンジャンル一覧画面作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,36 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genres = Genre.all
+    if @genre.save
+      flash.now[:notice] = "新しいゲームジャンルを作成しました"
+    end
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "ゲームジャンルの情報を変更しました"
+      redirect_to admin_genres_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
