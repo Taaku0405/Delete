e46c8eff4459c17367c3c833b253bb5b09d09461
Taaku0405 <rakosutaa@gmail.com>
2022-06-07 08:33:33 +0000

    アドミン側の会員機能を実装

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,18 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+  end 
+  
+  def show
+  end 
+  
+  def edit
+  end 
+  
+  def update
+  end 
+  
+  def destroy
+  end 
+  
 end
