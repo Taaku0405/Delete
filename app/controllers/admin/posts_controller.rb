8391db3a3213f6aec33836c5249e7d67af5a9fef
Taaku0405 <rakosutaa@gmail.com>
2022-06-08 06:24:10 +0000

    アドミン側投稿機能実装

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -5,15 +5,33 @@ class Admin::PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to admin_post_path(@post), notice: "ゲームの投稿情報を更新しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+   @post = Post.find(params[:id])
+   @post.destroy
+   redirect_to admin_posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :introduction, :genre_id)
   end
 
 end
