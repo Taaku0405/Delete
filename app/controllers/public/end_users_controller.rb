787aa6685cf24d22ec205df9618f84115727f97e
Taaku0405 <rakosutaa@gmail.com>
2022-06-09 06:58:09 +0000

    エンドユーザーの会員機能を実装

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -3,20 +3,44 @@ class Public::EndUsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
 
   def index
+    @end_user = EndUser.all
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
+    @end_user = current_end_user
+  end
+
+  def update
+    @end_user = current_end_user
+    if @end_user.update(end_user_params)
+      redirect_to end_user_path, notice: "会員情報を更新しました"
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+    @end_user = current_end_user
   end
 
   def withdraw
+    @end_user = current_end_user
+    #会員のis_deletedにtrueを入れて更新した時(退会しなかった時)
+    @end_user.update(deleted_flag: true)
+    #ログイン情報をリセットした時(退会した時)
+    reset_session
+    redirect_to root_path
   end
 
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name, :introduction, :profile_image, :email)
+  end
 
   def ensure_guest_user
     @end_user = EndUser.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_070902) do
+ActiveRecord::Schema.define(version: 2022_06_09_052049) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2022_06_03_070902) do
     t.boolean "is_deleted", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
